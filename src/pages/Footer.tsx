06f915e163101ef11b5206ffbbe013f4228dc2d3
Grayson Palermo <Palermo.4561@gmail.com>
2025-07-26 13:35:49 -0500

    fixed page link

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <Page className="bg-dark-red">
       <h1>Footer Page</h1>
-      <a href="vecteezy.com" className="text-light-blue underline">
+      <a href="https://www.vecteezy.com" target="_blank" className="text-light-blue underline">
         PNGs from Vecteezy
       </a>
     </Page>
