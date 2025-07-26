afd33b9d696ca3eaea180e5edfd4c6b49861a477
Grayson Palermo <Palermo.4561@gmail.com>
2025-07-26 01:51:30 -0500

    updating component import alias

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,5 @@
 import type { HTMLAttributes, ReactNode } from 'react'
-import { cn } from '../lib/utils'
+import { cn } from '@/lib/utils'
 
 interface PageProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode
