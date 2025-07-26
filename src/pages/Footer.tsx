afda4c50054390294177db7d3c6fe1153625fdb2
Grayson Palermo <Palermo.4561@gmail.com>
2025-07-26 00:20:32 -0500

    added images and credits

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -4,6 +4,9 @@ export default function Footer() {
   return (
     <Page className="bg-dark-red">
       <h1>Footer Page</h1>
+      <a href="vecteezy.com" className="text-light-blue underline">
+        PNGs from Vecteezy
+      </a>
     </Page>
   )
 }
