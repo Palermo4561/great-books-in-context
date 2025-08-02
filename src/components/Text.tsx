588e20576dd3e2642b1d0cc9ef5a090e551b275d
Grayson Palermo <Palermo.4561@gmail.com>
2025-08-02 01:31:07 -0500

    made text component responsive

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,4 +1,4 @@
-import type { HTMLAttributes, JSX, ReactNode } from 'react'
+import type { ElementType, HTMLAttributes, ReactNode } from 'react'
 import { tv } from 'tailwind-variants'
 
 import { cn } from '@/lib/utils'
@@ -10,7 +10,7 @@ import { cn } from '@/lib/utils'
 // p_sm = small paragraph
 type TextKey = 'headliner' | 'header' | 'subheader' | 'p_md' | 'p_sm'
 
-const TEXT_TYPE_MAP: Record<TextKey, keyof JSX.IntrinsicElements> = {
+const TEXT_TYPE_MAP: Record<TextKey, ElementType> = {
   headliner: 'h1',
   header: 'h2',
   subheader: 'h3',
@@ -19,15 +19,15 @@ const TEXT_TYPE_MAP: Record<TextKey, keyof JSX.IntrinsicElements> = {
 }
 
 const text = tv({
-  base: 'text-black text-center p-4',
+  base: 'text-black text-center xl:p-4 lg:p-3 md:p-2 p-1',
   variants: {
     variant: {
-      headliner: 'text-8xl',
-      header: 'text-7xl',
-      subheader: 'text-4xl',
-      p_md: 'text-3xl',
-      p_sm: 'text-2xl',
-    } as Record<TextKey, string>,
+      headliner: 'md:text-7xl xl:text-8xl sm:text-6xl lg:text-[5rem] text-4xl',
+      header: 'xl:text-7xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl',
+      subheader: 'xl:text-4xl lg:text-3xl md:text-2xl sm:text-xl text-lg',
+      p_md: 'xl:text-3xl lg:text-2xl md:text-xl text-sm',
+      p_sm: 'xl:text-2xl lg:text-xl md:text-base sm:text-sm text-xs',
+    },
   },
 })
 
@@ -40,9 +40,6 @@ export default function Text({ type, children, className, ...props }: TextProps)
   const Tag = TEXT_TYPE_MAP[type]
 
   return (
-    // this was yelling at me because of the Tag, but it works so I'm ignoring it
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     <Tag className={cn(text({ variant: type }), className)} {...props}>
       {children}
     </Tag>
