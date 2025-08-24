9580a5361d71c708eb1e4953c60d4062953fbaa3
Grayson Palermo <Palermo.4561@gmail.com>
2025-08-24 15:48:15 -0500

    feat: add image and mission text

--- src/pages/Mission.tsx
+++ src/pages/Mission.tsx
@@ -1,9 +1,24 @@
 import Page from '@/components/Page'
+import Text from '@/components/Text'
 
 export default function Mission() {
   return (
-    <Page>
-      <div className='w-full h-20' />
+    <Page className='p-0 relative'>
+      <img aria-label='Columbia University' className='relative' src='columbia_background.jpeg' />
+      <div className='absolute z-10 w-full h-full flex flex-col gap-4 justify-center items-center top-0 left-0'>
+        <Text type='header' className='underline font-bold'>
+          Our Mission
+        </Text>
+        <div className='p-6 w-1/2 bg-dark-blue opacity-85 rounded-4xl z-10 shadow-black shadow-lg'>
+          <Text type='p_md' className='opacity-100 font-bold text-black'>
+            For over a century, the Columbia Core Curriculum has exposed students to the Great Books of Western
+            literature and philosophy through small, discussion-based seminars. However, there are few opportunities for
+            students to hear directly from leading thinkers who engage critically with these foundational texts. Our
+            initiative aims to fill that gap by hosting lectures that place key works from the Literature Humanities and
+            Contemporary Civilization syllabi in broader intellectual and historical contexts.
+          </Text>
+        </div>
+      </div>
     </Page>
   )
 }
