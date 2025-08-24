3d7bfef80c180a075de3c75c0f4e24684a6a8636
Grayson Palermo <Palermo.4561@gmail.com>
2025-08-24 15:48:15 -0500

    feat: add image and mission text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Text from '@/components/Text'
 
 export default function Navbar() {
   return (
-    <div className='bg-dark-red fixed top-0 flex w-full flex-row justify-between opacity-95'>
+    <div className='bg-dark-red fixed top-0 flex w-full flex-row justify-between opacity-95 z-50'>
       <div className='flex flex-row content-center'>
         <img className='my-auto h-10 w-10' alt='Book icon' src='/book_icon.svg' />
         <Text type='p_sm' className='font-logo px-1 py-2'>
