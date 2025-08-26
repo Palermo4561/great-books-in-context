7150707dfca90e8674ab12cb0c7e87b757c977ae
Grayson Palermo <Palermo.4561@gmail.com>
2025-08-25 21:21:54 -0500

    style: clean up text input fields

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,8 +1,16 @@
-import { useState } from 'react'
+import { type ReactNode, useState } from 'react'
 
 import { postSubscriber } from '@/api/kit'
 import Page from '@/components/Page'
 import Text from '@/components/Text'
+
+const FormText = ({ children }: { children: ReactNode }) => {
+  return (
+    <Text type='p_sm' className='border-dark-red my-0 rounded-xl border-4 border-solid bg-white'>
+      {children}
+    </Text>
+  )
+}
 
 export default function SignUp() {
   const [email, setEmail] = useState('')
@@ -23,36 +31,39 @@ export default function SignUp() {
       <Text type='subheader'>Interested?</Text>
       <Text type='header'>Join our mailing list</Text>
       <Text type='p_md'>Stay updated on events and opportunities</Text>
-      <div className='border-dark-red mx-auto mt-8 mb-4 flex h-15 w-5/6 flex-col justify-center rounded-xl border-4 border-solid bg-white align-middle'>
-        <Text type='p_sm' className='text-gray bold mx-auto'>
-          Name and email input, and sign up button
-        </Text>
+      <div className='flex flex-col justify-center '>
+        <form onSubmit={submitForm} className='flex flex-row justify-center gap-10'>
+          <FormText>
+            <label>
+              Name:
+              <input
+                className='bg-white ml-3 focus:outline-0 text-gray-500'
+                type='text'
+                name='name'
+                placeholder='First Last'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+          </FormText>
+          <FormText>
+            <label>
+              Email:
+              <input
+                className='bg-white ml-3 focus:outline-0 text-gray-500 w-[400px]'
+                type='email'
+                name='email_address'
+                placeholder='example@domain.com'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </FormText>
+          <button type='submit' className='bg-dark-red rounded-xl font-bold text-2xl text-white px-3'>
+            Submit
+          </button>
+        </form>
       </div>
-      <form onSubmit={submitForm}>
-        <label>
-          Name:
-          <input
-            className='bg-white text-black'
-            type='text'
-            name='name'
-            placeholder='First Last'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            className='bg-white text-black'
-            type='email'
-            name='email_address'
-            placeholder='example@domain.com'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <button type='submit'>Submit</button>
-      </form>
 
       <p className='font-bold bg-black text-white text-2xl'>{outputText}</p>
     </Page>
