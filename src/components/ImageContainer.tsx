ff031269ac23d8b0619bde335c5ef52aeeaf4f00
Grayson Palermo <Palermo.4561@gmail.com>
2025-08-31 02:34:48 -0500

    refactor: delete unused file

--- src/components/ImageContainer.tsx
+++
@@ -1,9 +0,0 @@
-import type { HTMLAttributes } from 'react'
-
-interface ImageContainerProps extends HTMLAttributes<HTMLDivElement> {
-  imagePath: string
-}
-
-export default function ImageContainer({ imagePath, className, ...props }: ImageContainerProps) {
-  return <></>
-}
