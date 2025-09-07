c49cc9e508e3a14baf6bb3393a0f212f9545f563
Grayson Palermo <Palermo.4561@gmail.com>
2025-09-07 15:05:28 -0500

    feat: change text sizing in Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -43,33 +43,36 @@ export default function Contact() {
   }
 
   return (
-    <Page id='contact' className='flex flex-col gap-10'>
-      <Wrap>
-        <Text type='subheader'>Questions? Comments?</Text>
-        <div className='flex flex-row gap-3 justify-center items-center'>
-          <Text type='p_md'>
-            Contact Christian Bateman at{' '}
-            <a href={`mailto: ${email}`} className='text-light-blue underline'>
-              cab2364@columbia.edu
-            </a>
+    <Page id='contact'>
+      <div className='flex flex-col gap-5 my-[15vw]'>
+        <Wrap>
+          <Text type='subheader'>Questions? Comments?</Text>
+          <div className='flex flex-row gap-3 justify-center items-center'>
+            <Text type='p_md'>
+              Contact Christian Bateman at{' '}
+              <a href={`mailto: ${email}`} className='text-light-blue underline'>
+                cab2364@columbia.edu
+              </a>
+            </Text>
+            <button
+              type='button'
+              className='p-[1vw] m-y-auto h-fit cursor-pointer bg-light-blue rounded-2xl hover:text-gray-700 active:text-black'
+              title='Copy to Clipboard'
+              onClick={() => copyToClipboard()}
+            >
+              <ClipboardList />
+            </button>
+          </div>
+        </Wrap>
+        <Wrap className='bg-dark-tan flex flex-col gap-5'>
+          <Text type='header'>Thank you for your interest!</Text>
+          <Text type='p_md'>We hope to see you soon!</Text>
+          <Text type='p_sm'>
+            <span className='italic font-bold'>"The roots of education are bitter, but the fruit is sweet."</span> -
+            Aristotle, Diogenes Laertius <span className='italic'>Lives of Philosophers</span> bk. 5, sect. 18
           </Text>
-          <button
-            type='button'
-            className='p-[1vw] m-y-auto h-fit cursor-pointer bg-light-blue rounded-2xl hover:text-gray-700 active:text-black'
-            title='Copy to Clipboard'
-            onClick={() => copyToClipboard()}
-          >
-            <ClipboardList />
-          </button>
-        </div>
-      </Wrap>
-      <Wrap className='bg-dark-tan'>
-        <Text type='header'>Thank you for your interest!</Text>
-        <Text type='p_md'>We hope to see you at a lecture series event soon.</Text>
-        <Text className='mt-4' type='p_sm'>
-          <span className='italic'>"The roots of education are bitter, but the fruit is sweet."</span> - Laertius
-        </Text>
-      </Wrap>
+        </Wrap>
+      </div>
     </Page>
   )
 }
