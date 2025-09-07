cb0cccbf04a66313c59bac206b0d56b873d25cae
Grayson Palermo <Palermo.4561@gmail.com>
2025-09-07 12:10:30 -0500

    feat: dynamic sizing to Overview and Mission

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -10,23 +10,15 @@ export default function Overview() {
 
   return (
     <Page id='overview' className='mt-[15vw] flex flex-col gap-10'>
-      <FadeInSection>
-        <div className='bg-dark-tan border-5 px-2 py-1 shadow-2xl border-dark-red rounded-2xl w-full'>
-          <Text type='header'>The Core Curriculum</Text>
-          <Text type='header' className='font-bold italic'>
-            Reimagined
-          </Text>
-        </div>
-      </FadeInSection>
-      <FadeInSection>
-        <div className='grid grid-cols-3'>
+      <FadeInSection className='w-3/4 flex flex-col gap-5'>
+        <div className='flex flex-row justify-center-safe flex-wrap gap-5 '>
           {text.map((title, idx) => (
-            <OverviewItem title={title} src={images[idx]} description='' className='mx-2 my-5' />
+            <OverviewItem title={title} src={images[idx]} description='' className='w-100' />
           ))}
         </div>
       </FadeInSection>
       <FadeInSection>
-        <div className='bg-dark-tan border-5 px-2 py-1 shadow-2xl border-dark-red rounded-2xl'>
+        <div className='bg-dark-tan border-5 px-2  py-1 shadow-2xl border-dark-red rounded-2xl'>
           <Text type='p_md'>
             ...and <span className='font-bold italic'>much more</span> to engage with the Core Curriculum beyond the
             classroom
